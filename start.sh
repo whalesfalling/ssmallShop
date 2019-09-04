5a3150ebb601c3dd0d8cebbbfc4232234b5c14a7
whalesfalling <whalesfalling@163.com>
2019-09-04 17:33:12 +0800

    Linux自启脚本更新
    改动判断语句导致出现的BUG

--- start.sh
+++ start.sh
@@ -10,28 +10,30 @@
 #进程名字可修改
 PRO_NAME=litemall.jar
 time1=$(date)
-CMD=$(echo java -jar /home/weblogic/litemall.jar &)
 #用ps获取$PRO_NAME进程数量
 NUM=$(ps aux | grep -w ${PRO_NAME} | grep -v grep | wc -l)
 #少于1，重启进程
 if [[ ${NUM} -lt 1 ]];then
- echo "###### ${time1} ###### ${PRO_NAME} 进程不存在，自启程序启动！" >> /home/weblogic/start-log.txt
- ${CMD}
+ echo "###### ${time1} ###### ${PRO_NAME} 进程[${NUM}]不存在，自启程序启动！" >> /home/weblogic/start-log.txt
+ echo "------------------start---------------" >> /home/weblogic/start-log.txt
+ java -jar /home/weblogic/litemall.jar >> /home/weblogic/logs/log.log &
 #大于1，杀掉所有进程，重启
 elif [[ ${NUM} -gt 1 ]];then
-echo "###### ${time1} ###### ${PRO_NAME} 进程数异常，系统将杀死所有 ${PRO_NAME} 进程，然后重启！" >> /home/weblogic/start-log.txt
-ps aux | grep -w ${PRO_NAME} | grep -v grep | cut -c 9-15 | xargs kill -9
-${CMD}
+ echo "###### ${time1} ###### ${PRO_NAME} 进程数[${NUM}]异常，系统将杀死所有 ${PRO_NAME} 进程，然后重启！" >> /home/weblogic/start-log.txt
+ ps aux | grep -w ${PRO_NAME} | grep -v grep | cut -c 9-15 | xargs kill -9
+ echo "------------------start---------------" >> /home/weblogic/start-log.txt
+ java -jar /home/weblogic/litemall.jar >> /home/weblogic/logs/log.log &
 fi
 #kill僵尸进程
 NUM_STAT=$(ps aux | grep -w ${PRO_NAME} | grep T | grep -v grep | wc -l)
 if [[ ${NUM_STAT} -gt 0 ]];then
- echo "###### ${time1} ###### ${PRO_NAME} 进程僵死，系统将杀死 ${PRO_NAME} 进程，然后重启！" >> /home/weblogic/start-log.txt
+ echo "###### ${time1} ###### ${PRO_NAME} 进程[${NUM}]僵死，系统将杀死 ${PRO_NAME} 进程，然后重启！" >> /home/weblogic/start-log.txt
  ps aux | grep -w ${PRO_NAME} | grep -v grep | cut -c 9-15 | xargs kill -9
- ${CMD}
+ echo "------------------start---------------" >> /home/weblogic/start-log.txt
+ java -jar /home/weblogic/litemall.jar >> /home/weblogic/logs/log.log &
 fi
 # 进程正常
-if [[ ${NUM_STAT} -eq 0 ]];then
- echo "###### ${time1} ###### ${PRO_NAME} 进程正常运行中！" >> /home/weblogic/start-log.txt
+if [[ ${NUM} -eq 1 ]];then
+ echo "###### ${time1} ###### ${PRO_NAME} 进程[${NUM}]正常运行中！" >> /home/weblogic/start-log.txt
 fi
- 
+
